900aea77a01c6e31fd25129e14de4f864b748098
Jasmine Patel <jasmine.patel@footasylum.com>
2023-08-10 13:30:29 +0100

    Initial Commit

---
+++ NavigationSplitView/NavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewApp.swift
+//  NavigationSplitView
+//
+//  Created by Jasmine Patel on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
